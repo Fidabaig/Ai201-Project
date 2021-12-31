1929d2afff9384cead023d91d8cf36310b92692d
Fidabaig <93541892+Fidabaig@users.noreply.github.com>
2021-12-31 23:29:33 +0500

    Week2 progress commited

--- GAME1.py
+++ GAME1.py
@@ -13,6 +13,40 @@ BOARD_HEIGHT=600
 LINE_WIDTH=10
 BOARD_COLOR=(150,250,100)
 LINE_COLOR=(50,120,55)
+BOARD_ROWS=3
+BOARD_COLS=3
+CIRCLE_RADIUS = 40
+CIRCLE_WIDTH = 7
+CIRCLE_COLOR=(239,231,200)
+RED = (255, 0, 0)
+CROSS_WIDTH = 25
+SPACE = 55
+CROSS_COLOR = (66, 66, 66)
+board=np.zeros((BOARD_ROWS,BOARD_COLS))
+player=1
+#print(board)
+def mark_square(row, col, player):
+	board[row][col] = player
+
+def available_square(row, col):
+	return board[row][col] == 0
+
+def is_board_full():
+	for row in range(BOARD_ROWS):
+
+		for col in range(BOARD_COLS):
+			if board[row][col] == 0:
+				return False
+	return True
+def draw_figures():
+	for row in range(BOARD_ROWS):
+		for col in range(BOARD_COLS):
+			if board[row][col] == 1:
+				pygame.draw.circle( screen, CIRCLE_COLOR, (int( col  * 200 + 100 ), int( row * 200 + 100 )), CIRCLE_RADIUS, CIRCLE_WIDTH )
+			elif board[row][col] == 2:
+				pygame.draw.line(screen,CROSS_COLOR,(col *200 +SPACE,row *200+200-SPACE), (col * 200 + 200-SPACE,row * 200+SPACE ), CROSS_WIDTH )
+                pygame.draw.line(screen,CROSS_COLOR,(col *200 +SPACE,row * 200+ SPACE),(col * 200 + 200 - SPACE,row * 200 + 200  - SPACE), CROSS_WIDTH)
+
 def draw_line_4D():
     pygame.draw.line(screen,LINE_COLOR,(70,150),(530,150),LINE_WIDTH)
     pygame.draw.line(screen,LINE_COLOR,(70,300),(530,300),LINE_WIDTH)
@@ -42,4 +76,17 @@ while(True):
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             sys.exit()
+        if event.type==pygame.MOUSEBUTTONDOWN:
+            mouseX = event.pos[0]  # x_coordinate
+            mouseY = event.pos[1]  # y_coordinate
+            clicked_row = int(mouseY // 200)
+            clicked_col = int(mouseX // 200)
+            if available_square(clicked_row,clicked_col):
+                if player==1:
+                    mark_square(clicked_row,clicked_col,1)
+                    player=2
+                elif player==2:
+                    mark_square(clicked_row,clicked_col,2)
+                    player=1
+                draw_figures()
     pygame.display.update()
